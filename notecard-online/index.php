3884a36d8489e2aabef43730dec2cace7b7071eb
bergzinkernoppe <moser.vinzenz@outlook.de>
2020-07-25 19:00:32 +0200

    Docker & Ordner erstellt, Repo initialisiert

---
+++ notecard-online/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>Home - Notecard-Online</title>
+    </head>
+    <body>
+        <?php
+        echo "this is a test";
+        ?>
+    </body>
+</html>
