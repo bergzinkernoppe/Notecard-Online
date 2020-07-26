e164724232e728eb7315c8ba3a555ef6dfce10b4
LarsDrescher <larsdrescher60@gmail.com>
2020-07-26 18:55:06 +0200

    In index.php code eingefügt

--- index.php
+++ index.php
@@ -4,6 +4,24 @@
         <meta charset="utf-8" name="viewport" content="width=device-width,initial-scale=1.0">
     </head>
     <body>
-        <div>this is a test</div>
+        <div class="Title-Wrapper">
+            <div class="Title">
+                Notecard-Online
+            </div>
+        </div>
+        <div class="panel-wrapper">
+            <div class="panel" id="panel">
+            </div>
+			<div class="panel">
+			</div>
+		</div>
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg"><path id="wave" d=""/></svg>
+		<svg version="1.1" xmlns="http://www.w3.org/2000/svg"><path id="wave2" d=""/></svg>
+		<script src="node_modules/dist/jquery.js"></script>
+		<script src="node_modules/dist/gsap.min.js"></script>
+		<script src="node_modules/dist/wavify.js"></script>
+        <script src="node_modules/dist/anime.min.js"></script>
+		<script src="node_modules/dist/jquery.wavify.js"></script>
+        <script src="node_modules/dist/example.js"></script>
     </body>
 </html>
